fd769e9a55ebd9b36412c9b4b9f6fe7929eca863
bananakodira <ana.urlic@gmail.com>
2021-12-26 15:38:08 +0100

    Added Github link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      <div className="container">
+          <h1>Weather App </h1>
+        <footer> 
+            This project was coded by <a href="https://github.com/bananakodira" target="_blank" rel="noopener referrer" > 
+            Ana Urlic </a> and is <a href="https://github.com/bananakodira/banana-weather-app-react" target="_blank" rel="noopener referrer" > 
+        open sourced on GitHub</a> 
+        and <a href="https://wizardly-pare-10ddc2.netlify.app" target="_blank" rel="noopen referrer"> hosted on Netlify</a>
+        </footer>
+      </div>
+      </div>
+  ); }
---
+++ src/Weather.js
@@ -0,0 +1,5 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./Weather.css";
+
+export default function Weather(props) {}
