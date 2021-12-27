0f4dc69d872a819bf741f7a7269b5921f6941f1c
bananakodira <ana.urlic@gmail.com>
2021-12-27 14:51:35 +0100

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-          <h1>Weather App </h1>
+          <Weather/>
         <footer> 
-            This project was coded by <a href="https://github.com/bananakodira" target="_blank" rel="noopener referrer" > 
-            Ana Urlic </a> and is <a href="https://github.com/bananakodira/banana-weather-app-react" target="_blank" rel="noopener referrer" > 
-        open sourced on GitHub</a> 
-        and <a href="https://wizardly-pare-10ddc2.netlify.app" target="_blank" rel="noopen referrer"> hosted on Netlify</a>
+            This project was coded by <a href="https://github.com/bananakodira" target="_blank" rel="noreferrer"> Ana Urlic, </a> {""}{""} is {""}
+            <a href="https://github.com/bananakodira/banana-weather-app-react" target="_blank" rel="noreferrer" > 
+        open sourced on GitHub {""}</a> 
+        and <a href="https://wizardly-pare-10ddc2.netlify.app" target="_blank" rel="noreferrer"> hosted on Netlify</a>
         </footer>
       </div>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,55 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
 import "./Weather.css";
 
-export default function Weather(props) {}
+export default function Weather () {
+    return (
+        <div className="Weather">
+            <form> 
+                <div className="row"> 
+                <div className= "col-9">
+                <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on"/>
+                </div>
+                <div className="col-3"> 
+                <input type="submit" value= "Search" className= "btn btn-primary w-100"/>
+                </div>
+                </div>
+            </form>
+            <br/>
+        <div className="row">
+            <div className="col-4"> 
+    <img src=" https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+    alt="Partly Cloudy"/>
+    <span className= "temperature"> 12 </span>
+    <span className= "unit"> °C | F </span> 
+    </div>
+    <div className="col-3"> 
+    <div className="parameters">
+        Precipitation: 
+        <span className= "precipitation">  4 </span> % 
+        <br/>
+        Humidity:
+        <span className= "humidity">  84 </span> %
+        <br/>
+        Wind:
+        <span className= "wind"> 6 </span> km/h
+        </div>
+        </div>
+        <div className="col-5"> 
+        <h2> San Diego, CA, USA</h2> 
+        <ul> 
+            <li> 
+                Sunday 06:00
+            </li>
+            <li> 
+                Partly Cloudy
+            </li>
+        </ul>
+        </div>
+        </div>
+        <br/>
+        <div className="row">
+            <span > Temperature |  Precipitation   | Wind </span> 
+            </div> 
+             </div> 
+    )
+}
